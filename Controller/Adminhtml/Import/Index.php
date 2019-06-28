0b9b86075154aa4e56da59115c59ce11b1b99cd6
dnd-rodrigue <rodrigue.millard@dnd.fr>
2019-06-28 17:07:50 +0200

    acl-authorization: Fix acl check for system tan

--- Controller/Adminhtml/Import/Index.php
+++ Controller/Adminhtml/Import/Index.php
@@ -34,6 +34,6 @@ class Index extends Action
      */
     protected function _isAllowed()
     {
-        return $this->_authorization->isAllowed('Akeneo_Connector_Api::import');
+        return $this->_authorization->isAllowed('Akeneo_Connector::import');
     }
 }
