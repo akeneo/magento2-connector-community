6e1c017e1213c19eb6cba136d055e455d3fff10a
Rodrigue Millard <rodstar@dnd.fr>
2019-11-29 18:12:27 +0100

    existing-product-mapping: Add products already in magento to akeneo entities table to prevent doubles

--- Helper/Import/Product.php
+++ Helper/Import/Product.php
@@ -8,6 +8,8 @@ use Magento\Catalog\Model\Product as BaseProductModel;
 use Magento\Framework\App\DeploymentConfig;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\DB\Select;
+use Zend_Db_Expr as Expr;
 
 /**
  * Class Product
@@ -284,5 +286,130 @@ class Product extends Entities
         }
 
         return false;
+    }
+
+    /**
+     * Match Magento Id with code
+     *
+     * @param string $pimKey
+     * @param string $entityTable
+     * @param string $entityKey
+     * @param string $import
+     * @param string $prefix
+     *
+     * @return \Akeneo\Connector\Helper\Import\Product
+     */
+    public function matchEntity($pimKey, $entityTable, $entityKey, $import, $prefix = null)
+    {
+        /** @var \Magento\Framework\DB\Adapter\AdapterInterface $connection */
+        $connection = $this->connection;
+        /** @var string $tableName */
+        $tableName = $this->getTableName($import);
+
+        $connection->delete($tableName, [$pimKey . ' = ?' => '']);
+        /** @var string $akeneoConnectorTable */
+        $akeneoConnectorTable = $this->getTable('akeneo_connector_entities');
+        /** @var string $entityTable */
+        $entityTable = $this->getTable($entityTable);
+
+        if ($entityKey == 'entity_id') {
+            $entityKey = $this->getColumnIdentifier($entityTable);
+        }
+
+        /* Connect existing Magento products to new Akeneo items */
+        // Get existing entities from Akeneo table
+        /** @var Select $select */
+        $select = $connection->select()->from($akeneoConnectorTable, ['entity_id' => 'entity_id'])->where(
+            'import = ?',
+            'product'
+        );
+        /** @var string[] $existingEntities */
+        $existingEntities = $connection->query($select)->fetchAll();
+        $existingEntities = array_column($existingEntities, 'entity_id');
+
+        // Get all entities that are being imported and already present in Magento
+        $select = $connection->select()->from(['t' => $tableName], ['sku' => 't.identifier'])->joinInner(
+            ['e' => $entityTable],
+            't.identifier = e.sku'
+        );
+        /** @var string $query */
+        $query  = $connection->query($select);
+        /** @var mixed $row */
+        while ($row = $query->fetch()) {
+            // Create a row in Akeneo table for products present in Magento and Akeneo that were never imported before
+            if (!in_array($row['entity_id'], $existingEntities)) {
+                $values = [
+                    'import' => 'product',
+                    'code' => $row['sku'],
+                    'entity_id' => $row['entity_id'],
+                ];
+                $connection->insertOnDuplicate($akeneoConnectorTable, $values);
+            }
+        }
+
+        /* Update entity_id column from akeneo_connector_entities table */
+        $connection->query('
+            UPDATE `' . $tableName . '` t
+            SET `_entity_id` = (
+                SELECT `entity_id` FROM `' . $akeneoConnectorTable . '` c
+                WHERE ' . ($prefix ? 'CONCAT(t.`' . $prefix . '`, "_", t.`' . $pimKey . '`)' : 't.`' . $pimKey . '`') . ' = c.`code`
+                    AND c.`import` = "' . $import . '"
+            )
+        ');
+
+        /* Set entity_id for new entities */
+        /** @var string $query */
+        $query = $connection->query('SHOW TABLE STATUS LIKE "' . $entityTable . '"');
+        /** @var mixed $row */
+        $row = $query->fetch();
+
+        $connection->query('SET @id = ' . (int)$row['Auto_increment']);
+        /** @var array $values */
+        $values = [
+            '_entity_id' => new Expr('@id := @id + 1'),
+            '_is_new' => new Expr('1'),
+        ];
+        $connection->update($tableName, $values, '_entity_id IS NULL');
+
+        /* Update akeneo_connector_entities table with code and new entity_id */
+        /** @var Select $select */
+        $select = $connection->select()
+            ->from(
+                $tableName,
+                [
+                    'import' => new Expr("'" . $import . "'"),
+                    'code' => $prefix ? new Expr('CONCAT(`' . $prefix . '`, "_", `' . $pimKey . '`)') : $pimKey,
+                    'entity_id' => '_entity_id',
+                ]
+            )->where('_is_new = ?', 1);
+
+        $connection->query(
+            $connection->insertFromSelect($select, $akeneoConnectorTable, ['import', 'code', 'entity_id'], 2)
+        );
+
+        /* Update entity table auto increment */
+        /** @var string $count */
+        $count = $connection->fetchOne(
+            $connection->select()->from($tableName, [new Expr('COUNT(*)')])->where('_is_new = ?', 1)
+        );
+        if ($count) {
+            /** @var string $maxCode */
+            $maxCode = $connection->fetchOne(
+                $connection->select()
+                    ->from($akeneoConnectorTable, new Expr('MAX(`entity_id`)'))
+                    ->where('import = ?', $import)
+            );
+            /** @var string $maxEntity */
+            $maxEntity = $connection->fetchOne(
+                $connection->select()
+                    ->from($entityTable, new Expr('MAX(`' . $entityKey . '`)'))
+            );
+
+            $connection->query(
+                'ALTER TABLE `' . $entityTable . '` AUTO_INCREMENT = ' . (max((int)$maxCode, (int)$maxEntity) + 1)
+            );
+        }
+
+        return $this;
     }
 }
