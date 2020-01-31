27b57473caa74940a215abc60d541c8aaa52057c
Rodrigue Millard <rodstar@dnd.fr>
2020-01-31 17:56:47 +0100

    check-entities: Check and delete missing entities in akeneo entities table

--- Job/Product.php
+++ Job/Product.php
@@ -841,6 +841,29 @@ class Product extends Import
         $connection->query($query);
     }
 
+    /**
+     * Check already imported entities are still in Magento
+     *
+     * @return void
+     */
+    public function checkEntities()
+    {
+        /** @var AdapterInterface $connection */
+        $connection = $this->entitiesHelper->getConnection();
+        /** @var string $akeneoConnectorTable */
+        $akeneoConnectorTable = $this->entitiesHelper->getTable('akeneo_connector_entities');
+        /** @var string $entityTable */
+        $entityTable = $this->entitiesHelper->getTable('catalog_product_entity');
+        /** @var \Magento\Framework\DB\Select $selectExistingEntities */
+        $selectExistingEntities = $connection->select()->from($entityTable, 'entity_id');
+        /** @var string[] $existingEntities */
+        $existingEntities = array_column($connection->query($selectExistingEntities)->fetchAll(), 'entity_id');
+
+        $connection->delete(
+            $akeneoConnectorTable,
+            ['import = ?' => 'product', 'entity_id NOT IN (?)' => $existingEntities]
+        );
+    }
 
     /**
      * Match code with entity
