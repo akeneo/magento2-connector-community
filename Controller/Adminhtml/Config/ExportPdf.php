4a1dacd8e28e3c17756a33416fccc2a75609044f
Bastien Witczak <Bazak@dnd.fr>
2021-02-12 15:40:06 +0100

    export-configs-pdf: Add ConfigManagement in export controller

--- Controller/Adminhtml/Config/ExportPdf.php
+++ Controller/Adminhtml/Config/ExportPdf.php
@@ -4,23 +4,14 @@ declare(strict_types=1);
 
 namespace Akeneo\Connector\Controller\Adminhtml\Config;
 
-use Akeneo\Connector\Helper\Config;
-use Akeneo\Connector\Model\Source\Edition;
+use Akeneo\Connector\Model\Config\ConfigManagement;
 use Exception;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Filesystem\DirectoryList;
-use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\App\Response\Http\FileFactory;
 use Magento\Framework\App\ResponseInterface;
-use Magento\Framework\DB\Adapter\AdapterInterface;
-use Magento\Framework\Db\Select;
-use Zend_Pdf;
 use Zend_Pdf_Exception;
-use Zend_Pdf_Font;
-use Zend_Pdf_Page;
-use Zend_Pdf_Resource_Font;
-use Zend_Pdf_Style;
 
 /**
  * Class ExportPdf
@@ -40,44 +31,26 @@ class ExportPdf extends Action
      */
     protected $fileFactory;
     /**
-     * Description $resourceConnection field
+     * Description $configManagement field
      *
-     * @var ResourceConnection $resourceConnection
+     * @var ConfigManagement $configManagement
      */
-    protected $resourceConnection;
-    /**
-     * Description $sourceEdition field
-     *
-     * @var Edition $sourceEdition
-     */
-    protected $sourceEdition;
-    /**
-     * Description HIDDEN_FIELDS constant
-     *
-     * @var string[] HIDDEN_FIELDS
-     */
-    const HIDDEN_FIELDS = [
-        Config::AKENEO_API_BASE_URL,
-        Config::AKENEO_API_PASSWORD,
-        Config::AKENEO_API_USERNAME,
-        Config::AKENEO_API_CLIENT_ID,
-        Config::AKENEO_API_CLIENT_SECRET,
-    ];
+    protected $configManagement;
 
     /**
      * ExportPdf constructor
      *
-     * @param Context            $context
-     * @param FileFactory        $fileFactory
-     * @param ResourceConnection $resourceConnection
+     * @param Context          $context
+     * @param FileFactory      $fileFactory
+     * @param ConfigManagement $configManagement
      */
     public function __construct(
         Context $context,
         FileFactory $fileFactory,
-        ResourceConnection $resourceConnection
+        ConfigManagement $configManagement
     ) {
-        $this->fileFactory        = $fileFactory;
-        $this->resourceConnection = $resourceConnection;
+        $this->fileFactory      = $fileFactory;
+        $this->configManagement = $configManagement;
 
         parent::__construct($context);
     }
@@ -91,51 +64,10 @@ class ExportPdf extends Action
      */
     public function execute()
     {
-        /** @var Zend_Pdf $pdf */
-        $pdf = new Zend_Pdf();
-        /** @var Zend_Pdf_Page $page */
-        $page         = $pdf->newPage(Zend_Pdf_Page::SIZE_A4);
-        $pdf->pages[] = $page;
         /** @var string $fileName */
         $fileName = 'export.pdf';
 
-        /** @var AdapterInterface $connection */
-        $connection = $this->resourceConnection->getConnection();
-        /** @var Select $select */
-        $select = $connection->select()->from(
-            [
-                'ccd' => 'core_config_data',
-            ]
-        )->where('path like ?', '%akeneo%');
-
-        /** @var mixed[] $configs */
-        $configs = $connection->fetchAll($select);
-
-        $this->setPageStyle($page);
-        /** @var int $lastPosition */
-        $lastPosition = 710;
-
-        /**
-         * @var int      $index
-         * @var string[] $config
-         */
-        foreach ($configs as $index => $config) {
-            /** @var string $value */
-            $value = $config['path'] . ' : ';
-
-            if (in_array($config['path'], self::HIDDEN_FIELDS)) {
-                $value .= '****';
-            } else {
-                $value .= $config['value'];
-            }
-
-            if($config['path'] === Config::AKENEO_API_EDITION) {
-                $value = $this->getEdition();
-            }
-
-            $page->drawText($value, 100, $lastPosition);
-            $lastPosition -= 10;
-        }
+        $pdf = $this->configManagement->generatePdf();
 
         return $this->fileFactory->create(
             $fileName,
@@ -143,37 +75,5 @@ class ExportPdf extends Action
             DirectoryList::VAR_DIR,
             'application/pdf'
         );
-    }
-
-    /**
-     * Description setPageStyle function
-     *
-     * @param Zend_Pdf_Page $page
-     *
-     * @return mixed
-     * @throws Zend_Pdf_Exception
-     */
-    protected function setPageStyle($page)
-    {
-        /** @var Zend_Pdf_Style $style */
-        $style = new Zend_Pdf_Style();
-        $style->setLineColor(new \Zend_Pdf_Color_Rgb(0, 0, 0));
-        /** @var Zend_Pdf_Resource_Font $font */
-        $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_TIMES);
-        $style->setFont($font, 5);
-        $page->setStyle($style);
-
-        return $page;
-    }
-
-    /**
-     * Description getEdition function
-     *
-     * @return string
-     */
-    protected function getEdition()
-    {
-        $version = '';
-        return $version;
     }
 }
