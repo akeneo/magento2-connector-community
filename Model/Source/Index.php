f506b07b83c6c28a2aae25a4dc1a999e6c0278fb
Rémi Verrière <gimix@dnd.fr>
2021-08-20 10:57:45 +0200

    add-cache-dump-and-indexation: Add Dynamic Index collection for admin configuration Source Model

--- Model/Source/Index.php
+++ Model/Source/Index.php
@@ -2,18 +2,8 @@
 
 namespace Akeneo\Connector\Model\Source;
 
-use Magento\Catalog\Model\Indexer\Category\Product;
-use Magento\Catalog\Model\Indexer\Product\Category;
-use Magento\Catalog\Model\Indexer\Product\Eav\Processor;
-use Magento\Catalog\Model\Indexer\Product\Price\Processor as PriceProcessor;
-use Magento\CatalogInventory\Model\Indexer\Stock\Processor as StockProcessor;
-use Magento\CatalogRule\Model\Indexer\Product\ProductRuleProcessor;
-use Magento\CatalogRule\Model\Indexer\Rule\RuleProductProcessor;
-use Magento\CatalogSearch\Model\Indexer\Fulltext;
-use Magento\Customer\Model\Customer;
 use Magento\Framework\Option\ArrayInterface;
-use Magento\InventoryIndexer\Indexer\InventoryIndexer;
-use Magento\Theme\Model\Data\Design\Config;
+use Magento\Indexer\Ui\DataProvider\Indexer\DataCollection;
 
 /**
  * Class Index
@@ -26,57 +16,43 @@ use Magento\Theme\Model\Data\Design\Config;
 class Index implements ArrayInterface
 {
     /**
+     * Data Collection
+     *
+     * @var DataCollection $dataCollection
+     */
+    private $dataCollection;
+
+    /**
+     * Index constructor
+     *
+     * @param DataCollection $dataCollection
+     */
+    public function __construct(
+        DataCollection $dataCollection
+    ) {
+        $this->dataCollection = $dataCollection;
+    }
+
+    /**
      * Return array of options for the index
      *
      * @return array Format: array('<value>' => '<label>', ...)
      */
     public function toOptionArray()
     {
-        return [
-            [
-                'label' => __('design_config_grid'),
-                'value' => Config::DESIGN_CONFIG_GRID_INDEXER_ID,
-            ],
-            [
-                'label' => __('customer_grid'),
-                'value' => Customer::CUSTOMER_GRID_INDEXER_ID,
-            ],
-            [
-                'label' => __('catalog_category_product'),
-                'value' => Product::INDEXER_ID,
-            ],
-            [
-                'label' => __('catalog_product_category'),
-                'value' => Category::INDEXER_ID,
-            ],
-            [
-                'label' => __('catalogrule_rule'),
-                'value' => RuleProductProcessor::INDEXER_ID,
-            ],
-            [
-                'label' => __('catalog_product_attribute'),
-                'value' => Processor::INDEXER_ID,
-            ],
-            [
-                'label' => __('cataloginventory_stock'),
-                'value' => StockProcessor::INDEXER_ID,
-            ],
-            [
-                'label' => __('inventory'),
-                'value' => InventoryIndexer::INDEXER_ID,
-            ],
-            [
-                'label' => __('catalogrule_product'),
-                'value' => ProductRuleProcessor::INDEXER_ID,
-            ],
-            [
-                'label' => __('catalog_product_price'),
-                'value' => PriceProcessor::INDEXER_ID,
-            ],
-            [
-                'label' => __('catalogsearch_fulltext'),
-                'value' => Fulltext::INDEXER_ID,
-            ],
-        ];
+        /** @var string[] $indexerOptions */
+        $indexerOptions = [];
+        /** @var mixed[] $indexCollection */
+        $indexCollection = $this->dataCollection->loadData();
+
+        /** @var string[] $indexerData */
+        foreach ($indexCollection as $indexerData) {
+            $indexerOptions[] = [
+                'label' => $indexerData['title'],
+                'value' => $indexerData['indexer_id'],
+            ];
+        }
+
+        return $indexerOptions;
     }
 }
