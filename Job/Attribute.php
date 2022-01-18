1ed119223b67e714a7e301ee12e86f07352efef0
dnd-badong <gautier.bardon@dnd.fr>
2022-01-18 14:27:28 +0100

    AKCOMAG-204: fix wrong scope for newly imported attributes

--- Job/Attribute.php
+++ Job/Attribute.php
@@ -569,10 +569,10 @@ class Attribute extends Import
             /* Retrieve attribute scope */
             /** @var int $global */
             $global = ScopedAttributeInterface::SCOPE_GLOBAL; // Global
-            if ((int) $row['scopable'] === 1) {
+            if ((int)$row['scopable'] === 1) {
                 $global = ScopedAttributeInterface::SCOPE_WEBSITE; // Website
             }
-            if ((int) $row['localizable'] === 1 || $row['type'] === self::PIM_CATALOG_TABLE) {
+            if ((int)$row['localizable'] === 1 || $row['type'] === self::PIM_CATALOG_TABLE) {
                 $global = ScopedAttributeInterface::SCOPE_STORE; // Store View
             }
             /** @var array $data */
