f4b132532f214bfd44d63038f81596ba4cb729f4
dnd-badong <gautier.bardon@dnd.fr>
2022-01-21 09:19:22 +0100

    AKCOMAG2001-203: Fix review

--- Setup/Patch/Data/UpdateConfigurableAttributes.php
+++ Setup/Patch/Data/UpdateConfigurableAttributes.php
@@ -14,10 +14,9 @@ use Magento\Framework\Setup\Patch\DataPatchInterface;
 /**
  * Class UpdateConfigurableAttributes
  *
- * @category  Class
  * @package   Akeneo\Connector\Setup
  * @author    Agence Dn'D <contact@dnd.fr>
- * @copyright 2010-present Agence Dn'D
+ * @copyright 2004-present Agence Dn'D
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      https://www.dnd.fr/
  */
