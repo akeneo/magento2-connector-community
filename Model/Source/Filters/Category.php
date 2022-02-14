7aa1db454097b77da6d087d0a967b8cc2070ab40
dnd-badong <gautier.bardon@dnd.fr>
2022-02-14 15:05:45 +0100

    AKCOMAG2001-213: use "is_root" category filter if available

--- Model/Source/Filters/Category.php
+++ Model/Source/Filters/Category.php
@@ -2,12 +2,15 @@
 
 namespace Akeneo\Connector\Model\Source\Filters;
 
+use Akeneo\Connector\Helper\Authenticator;
 use Akeneo\Connector\Helper\Config as ConfigHelper;
+use Akeneo\Connector\Model\Source\Edition;
 use Akeneo\Pim\ApiClient\AkeneoPimClientInterface;
 use Akeneo\Pim\ApiClient\Pagination\ResourceCursorInterface;
+use Akeneo\Pim\ApiClient\Search\SearchBuilderFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Option\ArrayInterface;
 use Psr\Log\LoggerInterface as Logger;
-use Akeneo\Connector\Helper\Authenticator;
 
 /**
  * Class Category
@@ -21,6 +24,21 @@ use Akeneo\Connector\Helper\Authenticator;
  */
 class Category implements ArrayInterface
 {
+    /**
+     * Akeneo pim edition config path
+     *
+     * @var string AKENEO_PIM_EDITION_CONFIG_PATH
+     */
+    public const AKENEO_PIM_EDITION_CONFIG_PATH = 'akeneo_connector/akeneo_api/edition';
+    /**
+     * Versions for which "is_root" property exists on categories
+     *
+     * @var string[] IS_ROOT_ENABLED_VERSIONS
+     */
+    public const IS_ROOT_ENABLED_VERSIONS = [
+        Edition::SERENITY,
+        Edition::GROWTH
+    ];
     /**
      * This variable is used for Akeneo Authenticator
      *
@@ -39,22 +57,40 @@ class Category implements ArrayInterface
      * @var ConfigHelper $configHelper
      */
     protected $configHelper;
+    /**
+     * Scope config
+     *
+     * @var ScopeConfigInterface $scopeConfig
+     */
+    private $scopeConfig;
+    /**
+     * Akeneo search builder factory
+     *
+     * @var SearchBuilderFactory $searchBuilderFactory
+     */
+    private $searchBuilderFactory;
 
     /**
      * Category constructor
      *
-     * @param Authenticator $akeneoAuthenticator
-     * @param Logger        $logger
-     * @param ConfigHelper  $configHelper
+     * @param Authenticator        $akeneoAuthenticator
+     * @param Logger               $logger
+     * @param ConfigHelper         $configHelper
+     * @param ScopeConfigInterface $scopeConfig
+     * @param SearchBuilderFactory $searchBuilderFactory
      */
     public function __construct(
         Authenticator $akeneoAuthenticator,
         Logger $logger,
-        ConfigHelper $configHelper
+        ConfigHelper $configHelper,
+        ScopeConfigInterface $scopeConfig,
+        SearchBuilderFactory $searchBuilderFactory
     ) {
-        $this->akeneoAuthenticator = $akeneoAuthenticator;
-        $this->logger              = $logger;
-        $this->configHelper        = $configHelper;
+        $this->akeneoAuthenticator  = $akeneoAuthenticator;
+        $this->logger               = $logger;
+        $this->configHelper         = $configHelper;
+        $this->scopeConfig          = $scopeConfig;
+        $this->searchBuilderFactory = $searchBuilderFactory;
     }
 
     /**
@@ -75,14 +111,36 @@ class Category implements ArrayInterface
             }
             /** @var string|int $paginationSize */
             $paginationSize = $this->configHelper->getPaginationSize();
-            /** @var ResourceCursorInterface $categories */
-            $akeneoCategories = $client->getCategoryApi()->all($paginationSize);
-            /** @var mixed[] $category */
-            foreach ($akeneoCategories as $category) {
-                if (!isset($category['code']) || isset($category['parent'])) {
-                    continue;
+            /** @var string $akeneoPimVersion */
+            $akeneoPimVersion = $this->scopeConfig->getValue(self::AKENEO_PIM_EDITION_CONFIG_PATH);
+
+            if (in_array($akeneoPimVersion, self::IS_ROOT_ENABLED_VERSIONS)) {
+                /** @var mixed[][] $isRootFilter */
+                $isRootFilter = $this->searchBuilderFactory->create()
+                    ->addFilter('is_root', '=', true)
+                    ->getFilters();
+                /** @var ResourceCursorInterface $categories */
+                $akeneoCategories = $client->getCategoryApi()->all(
+                    $paginationSize,
+                    ['search' => $isRootFilter]
+                );
+                /** @var mixed[] $category */
+                foreach ($akeneoCategories as $category) {
+                    if (!isset($category['code'])) {
+                        continue;
+                    }
+                    $categories[$category['code']] = $category['code'];
                 }
-                $categories[$category['code']] = $category['code'];
+            } else {
+                /** @var ResourceCursorInterface $categories */
+                $akeneoCategories = $client->getCategoryApi()->all($paginationSize);
+                /** @var mixed[] $category */
+                foreach ($akeneoCategories as $category) {
+                    if (!isset($category['code']) || isset($category['parent'])) {
+                        continue;
+                    }
+                    $categories[$category['code']] = $category['code'];
+                }
             }
         } catch (\Exception $exception) {
             $this->logger->warning($exception->getMessage());
