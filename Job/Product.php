4059844c271d93407a99452dfe817fa59865d5ae
dnd-badong <gautier.bardon@dnd.fr>
2022-02-14 15:49:43 +0100

    AKCOMAG2001-209: rename variable

--- Job/Product.php
+++ Job/Product.php
@@ -3742,8 +3742,8 @@ class Product extends JobImport
         }
         /** @var string[] $families */
         $families = [];
-        /** @var string[] $familiesIN */
-        $familiesIN = [];
+        /** @var string[] $familiesIn */
+        $familiesIn = [];
         /** @var string|int $paginationSize */
         $paginationSize = $this->configHelper->getPaginationSize();
         /** @var string[] $apiFamilies */
@@ -3792,7 +3792,7 @@ class Product extends JobImport
 
                     if (isset($familyFilter['operator']) && $familyFilter['operator'] === 'IN') {
                         foreach ($familyFilter['value'] as $familyToKeep) {
-                            $familiesIN[] = $familyToKeep;
+                            $familiesIn[] = $familyToKeep;
                         }
                     }
                 }
@@ -3813,7 +3813,7 @@ class Product extends JobImport
 
         $families = array_values($families);
 
-        return $familiesIN ?: $families;
+        return $familiesIn ?: $families;
     }
 
     /**
